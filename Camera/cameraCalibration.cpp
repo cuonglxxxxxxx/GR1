c6a93644cc00931559db30942df6be6570c8393e
cuonglxxxxxxx <147698453+cuonglxxxxxxx@users.noreply.github.com>
2025-07-01 06:34:51 +0700

    Camera update

--- Camera/cameraCalibration.cpp
+++ Camera/cameraCalibration.cpp
@@ -4,85 +4,70 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <stdio.h>
 #include <iostream>
- 
-// Defining the dimensions of checkerboard
-int CHECKERBOARD[2]{6,9}; 
- 
+
+// Kích thước của bàn cờ
+int CHECKERBOARD[2]{6,9};
+
 int main()
 {
-  // Creating vector to store vectors of 3D points for each checkerboard image
+  // Vector danh sách các điểm 3D trong không gian(1 vector/ảnh)
   std::vector<std::vector<cv::Point3f> > objpoints;
- 
-  // Creating vector to store vectors of 2D points for each checkerboard image
+
+  // Vector danh sách các điểm 2D trong ảnh (1 vector/ảnh)
   std::vector<std::vector<cv::Point2f> > imgpoints;
- 
-  // Defining the world coordinates for 3D points
+  // Tạo tọa độ các điểm 3D trong không gian thế giới (nằm trên mặt phẳng z=0)
   std::vector<cv::Point3f> objp;
   for(int i{0}; i<CHECKERBOARD[1]; i++)
   {
     for(int j{0}; j<CHECKERBOARD[0]; j++)
       objp.push_back(cv::Point3f(j,i,0));
   }
- 
- 
-  // Extracting path of individual image stored in a given directory
+
   std::vector<cv::String> images;
-  // Path of the folder containing checkerboard images
   std::string path = "/home/cuong/Downloads/MonoVo/Data/setUpCamImages/*.png";
   cv::glob(path, images);
- 
+
   cv::Mat frame, gray;
-  // vector to store the pixel coordinates of detected checker board corners 
+  // Vector tọa độ pixel của các góc của bản cờ được phát hiện
   std::vector<cv::Point2f> corner_pts;
   bool success;
- 
-  // Looping over all the images in the directory
+
+  // Lặp qua tất cả ảnh 
   for(int i{0}; i<images.size(); i++)
   {
     frame = cv::imread(images[i]);
     cv::cvtColor(frame,gray,cv::COLOR_BGR2GRAY);
- 
-    // Finding checker board corners
-    // If desired number of corners are found in the image then success = true  
-   success = cv::findChessboardCorners(gray, cv::Size(CHECKERBOARD[0], CHECKERBOARD[1]), corner_pts,
+
+    // Nếu tìm đủ số góc mong muốn thì success = true
+    success = cv::findChessboardCorners(gray, cv::Size(CHECKERBOARD[0], CHECKERBOARD[1]), corner_pts,
             cv::CALIB_CB_ADAPTIVE_THRESH | cv::CALIB_CB_FAST_CHECK | cv::CALIB_CB_NORMALIZE_IMAGE);
 
-  cv::TermCriteria criteria(cv::TermCriteria::EPS | cv::TermCriteria::MAX_ITER, 30, 0.001);
-    /* 
-     * If desired number of corner are detected,
-     * we refine the pixel coordinates and display 
-     * them on the images of checker board
-    */
+    cv::TermCriteria criteria(cv::TermCriteria::EPS | cv::TermCriteria::MAX_ITER, 30, 0.001);
+
     if(success)
     {
-    //   cv::TermCriteria criteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 30, 0.001);
       cv::TermCriteria criteria(cv::TermCriteria::EPS | cv::TermCriteria::MAX_ITER, 30, 0.001);
-      // refining pixel coordinates for given 2d points.
+      // Tinh chỉnh tọa độ pixel của các điểm 2D
       cv::cornerSubPix(gray,corner_pts,cv::Size(11,11), cv::Size(-1,-1),criteria);
-       
-      // Displaying the detected corner points on the checker board
+
+      // Vẽ các điểm góc phát hiện được lên ảnh
       cv::drawChessboardCorners(frame, cv::Size(CHECKERBOARD[0], CHECKERBOARD[1]), corner_pts, success);
-       
+
       objpoints.push_back(objp);
       imgpoints.push_back(corner_pts);
     }
- 
+
     cv::imshow("Image",frame);
     cv::waitKey(0);
   }
- 
+
   cv::destroyAllWindows();
- 
-  cv::Mat cameraMatrix,distCoeffs,R,T;
- 
-  /*
-   * Performing camera calibration by 
-   * passing the value of known 3D points (objpoints)
-   * and corresponding pixel coordinates of the 
-   * detected corners (imgpoints)
-  */
+
+  cv::Mat cameraMatrix, distCoeffs, R, T;
+
+  // Hiệu chỉnh camera để tìm ma trận nội tại, độ méo, ma trận xoay và tịnh tiến
   cv::calibrateCamera(objpoints, imgpoints, cv::Size(gray.rows,gray.cols), cameraMatrix, distCoeffs, R, T);
- 
+
   std::cout << "cameraMatrix : " << cameraMatrix << std::endl;
   return 0;
 }
